2997430f4d6404e0852d2b4ac2b93bcc60b928dd
Udipikrishna Prasad <judipikrishnaprasad@gmail,com>
2025-08-16 22:23:18 +0530

    headless-mode

--- src/test/java/org/example/TC_LoginTest.java
+++ src/test/java/org/example/TC_LoginTest.java
@@ -3,13 +3,16 @@ package org.example;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class TC_LoginTest {
     @Test
     public void userLoginTest() {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
         driver.get("http://google.com");
         driver.close();
     }
